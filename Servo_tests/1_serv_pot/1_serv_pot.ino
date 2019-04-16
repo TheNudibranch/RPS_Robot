f3bcdc403a12c0525269685df99d0af5146234a7
Ian Costley <isc1@geneseo.edu>
2019-04-16 16:59:52 -0400

    Tested all servo pins

--- Servo_tests/1_serv_pot/1_serv_pot.ino
+++ Servo_tests/1_serv_pot/1_serv_pot.ino
@@ -1,3 +1,6 @@
+// The pins that have been tested and are availabl to control the servo are:
+// [10,9,6,5,3]
+
 #include <Servo.h>
 
 Servo myServo;
@@ -7,7 +10,7 @@ int potVal;
 int angle;
 
 void setup() {
-  myServo.attach(9); // Assign the servo pin to 9
+  myServo.attach(3); // assign pin
 
   Serial.begin(9600);
 }
