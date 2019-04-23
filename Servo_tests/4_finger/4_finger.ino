f6278edfa513a1f8bcf985941420be8f3bc4649c
Ian Costley <isc1@geneseo.edu>
2019-04-23 15:26:21 -0400

    Test for actual finger motion

---
+++ Servo_tests/4_finger/4_finger.ino
@@ -0,0 +1,61 @@
+// Test for the three different modes of the hand
+
+#include <Servo.h>
+
+Servo middle;
+Servo ring;
+Servo pinky;
+Servo index;
+Servo thumb;
+
+int servo_ar[5] = {10,9,6,5,3};
+int mode = 0;
+
+void setup() {
+  middle.attach(servo_ar[0]);
+  ring.attach(servo_ar[1]);
+  pinky.attach(servo_ar[2]);
+  index.attach(servo_ar[3]);
+  thumb.attach(servo_ar[4]);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()){
+    int inter = Serial.parseInt();
+    if (inter != 0){
+      mode = inter;
+    }
+  } 
+  changeFunctions(mode);
+  Serial.println(mode);
+}
+
+//////////////////// Functions
+
+void changeFunctions(int mode){
+  if (mode == 1){
+    // Nothing happens or paper
+    index.write(5);
+    middle.write(5);
+    ring.write(5);
+    pinky.write(5);
+    thumb.write(5);
+  }
+  
+  else if (mode == 2){
+    index.write(160);
+    middle.write(160);
+    ring.write(160);
+    pinky.write(160);
+    thumb.write(120);
+  }
+
+  else if (mode == 3){
+    index.write(5);
+    middle.write(5);
+    ring.write(160);
+    pinky.write(160);
+    thumb.write(90);
+  }
+}
